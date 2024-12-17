a2a64fd1d1459d4e7bed5f79f745a0b847facfe3
@jainil@ <jainilthummar2005@gmail.com>
2024-12-17 21:15:32 +0530

    17-12-24

---
+++ .net/lab-4/Hospital.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_4
+{
+    public class Hospital
+    {
+        public virtual void HospitalDetails()
+        {
+            Console.WriteLine("hospital details");
+        }
+    }
+    public class Appolo:Hospital
+    {
+        public override void HospitalDetails()
+        {
+            Console.WriteLine("Appolo details");
+        }
+    }
+    public class Wockhardt : Hospital
+    {
+        public override void HospitalDetails()
+        {
+            Console.WriteLine("Wockhardt details");
+        }
+    }
+    public class Gokul_Superspeciality : Hospital
+    {
+        public override void HospitalDetails()
+        {
+            Console.WriteLine("Gokul_Superspeciality details");
+        }
+    }
+}
---
+++ .net/lab-4/lab_4_b5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_4
+{
+    public class lab_4_b5
+    {
+        public int Area(int l)
+        {
+            return (l * l);
+        }
+        public int Area(int l, int b)
+        {
+            return (b * l);
+        }
+        public double Area(double l)
+        {
+            return (3.14 * l * l);
+        }
+    }
+}
