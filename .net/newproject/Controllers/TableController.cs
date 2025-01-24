34043437d574fed13a481a87d3d32e2e9b5bb442
@jainil@ <jainilthummar2005@gmail.com>
2025-01-24 08:26:43 +0530

    24-1-25

---
+++ .net/newproject/Controllers/TableController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace newproject.Controllers
+{
+    public class TableController : Controller
+    {
+        public IActionResult Table1()
+        {
+            return View();
+        }
+        public IActionResult Table2()
+        {
+            return View();
+        }
+    }
+}
