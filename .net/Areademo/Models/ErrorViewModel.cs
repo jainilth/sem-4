b0db387b7de9fa69e7c442e480e2ce866b308186
@jainil@ <jainilthummar2005@gmail.com>
2025-06-25 08:12:45 +0530

    '25-6-25'

--- .net/Areademo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Areademo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- .net/demo/obj/Debug/net8.0/demo.AssemblyInfo.cs
+++ .net/demo/obj/Debug/net8.0/demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f542964bf8c661e8bf4c51e1b72c710a40ec5329")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d980d5732acf173201067b0588ea97da1505dc24")]
 [assembly: System.Reflection.AssemblyProductAttribute("demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
